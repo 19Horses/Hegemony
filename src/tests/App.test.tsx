a7bd7d8d777ab23581a8b0b556d1ba9d16e7a0dc
Campbell Docherty <campbellsofitsidocherty@gmail.com>
2025-02-25 11:09:37 +0000

    Tests

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -1,10 +1,16 @@
 import { describe, test } from 'vitest';
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import App from '../App';
 
 describe('When the app renders', () => {
   test('it shows the title', () => {
     render(<App />);
     screen.getByText('Working Class London, Got Suttn to Say:');
+  });
+
+  test('it renders the posters', () => {
+    render(<App />);
+    screen.getByAltText('Tek charge Hegemony poster');
+    screen.getByAltText('Tracksuits and securing the bag Hegemony poster');
   });
 });
