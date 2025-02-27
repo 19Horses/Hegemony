bf705f60b1cb47f4ac6e245ffa155c56e9a41c0e
Campbell Docherty <campbellsofitsidocherty@gmail.com>
2025-02-27 16:02:47 +0000

    Scroll bar on both sides and 50/50 colour

--- src/ProgressBar.tsx
+++ src/ProgressBar.tsx
@@ -3,10 +3,16 @@ import { motion, useScroll } from 'framer-motion';
 const ProgressBar = () => {
   const { scrollYProgress } = useScroll();
   return (
-    <motion.div
-      className="progressBar"
-      style={{ scaleY: scrollYProgress }}
-    ></motion.div>
+    <>
+      <motion.div
+        className="progressBar left"
+        style={{ scaleY: scrollYProgress }}
+      ></motion.div>
+      <motion.div
+        className="progressBar"
+        style={{ scaleY: scrollYProgress }}
+      ></motion.div>
+    </>
   );
 };
 
